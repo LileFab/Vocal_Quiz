bf904e09fc2926f53b622cf6681dd9a89c06246e
Fabien <backspeen@gmail.com>
2024-04-25 16:30:42 +0200

    add bug report

--- nextjs/sentry.client.config.ts
+++ nextjs/sentry.client.config.ts
@@ -21,6 +21,11 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+      isEmailRequired: true,
+    }),
     Sentry.replayIntegration({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
