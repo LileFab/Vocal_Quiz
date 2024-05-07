cbe0463d31b9c99b5c44481c0370939b08e1feea
Andréa Joly <andrea.joly1989@gmail.com>
2024-05-07 09:41:44 +0200

    make architectural changes

--- nextjs/app/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-export const variants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1 },
-  exit: { opacity: 0 },
-};
--- nextjs/app/interface/UserResponse.ts
+++
@@ -1,9 +0,0 @@
-export interface UsersResponse {
-  question_id: number;
-  user_id: string | null;
-  user_response: string;
-  is_correct: boolean;
-  time_to_respond: number;
-  creation_date: Date | null;
-}
-
