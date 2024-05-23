af36378fe5f6f5e07539781e99ad088c6787cb6f
Andreaj42 <andrea.joly1989@gmail.com>
2024-05-23 23:14:20 +0200

    :art: pep8 compliant

--- model/notebooks/utils/plot_torchaudio.py
+++ model/notebooks/utils/plot_torchaudio.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import torch
 import librosa
 
+
 def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None):
     waveform = waveform.numpy()
 
@@ -19,7 +20,8 @@ def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None):
         if xlim:
             axes[c].set_xlim(xlim)
     figure.suptitle(title)
-    
+
+
 def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
     waveform = waveform.numpy()
 
@@ -35,14 +37,16 @@ def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
         if xlim:
             axes[c].set_xlim(xlim)
     figure.suptitle(title)
-    
-def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Spectrogram (db)')
-  axs.set_ylabel(ylabel)
-  axs.set_xlabel('frame')
-  im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
-  if xmax:
-    axs.set_xlim((0, xmax))
-  fig.colorbar(im, ax=axs)
-  plt.show(block=False)
+
+
+def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto',
+                     xmax=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or 'Spectrogram (db)')
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel('frame')
+    im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(im, ax=axs)
+    plt.show(block=False)
