7c8f693f1ff1eb63d284e921382b934a8ed89907
Fabien <backspeen@gmail.com>
2024-05-24 18:05:56 +0200

    🚑 Fix port API

--- backend/app/questions/components/QuestionCell.tsx
+++ backend/app/questions/components/QuestionCell.tsx
@@ -181,7 +181,7 @@ const QuestionCell = ({
         formData.append("file", blob, "recording.wav");
 
         try {
-            const response = await axios.post("http://localhost:5005/speech_to_text", formData, {
+            const response = await axios.post("http://localhost:5000/speech_to_text", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                     "Access-Control-Allow-Origin": "*",
