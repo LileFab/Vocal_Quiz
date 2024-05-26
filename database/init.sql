fcebc9e832ed528223e2a9d4d8d0ef422f3cf5f1
Andreaj42 <andrea.joly1989@gmail.com>
2024-05-26 21:47:20 +0200

    🗑️ remove unused user

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,1 @@
-CREATE USER superdevuser;
 CREATE DATABASE superdevdb;
-GRANT ALL PRIVILEGES ON DATABASE superdevdb TO superdevuser;
-ALTER USER superdevuser with PASSWORD 'P@ssword1';
-ALTER USER postgres with PASSWORD 'P@ssword1';
